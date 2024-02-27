df3436ea7e2dae1801c65b45d81fe39efae80b74
Pascalis Sandi <pascalis.sandi09@gmail.com>
2024-02-28 05:34:06 +0700

    Upload Jobsheet

---
+++ app/Http/Controllers/UserProfileController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserProfileController extends Controller
+{
+    function show(){
+        return 'Nama    : Pascalis D. S. L.<br>NIM  : 2241720140';
+    }
+}
